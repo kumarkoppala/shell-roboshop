f66a27ddcf43dcfe544dc2d2209aaff91f071360
joindevopscloud <info@joindevops.com>
2026-05-26 08:14:20 +0530

    api doc added

--- 03-mysql.sh
+++ 03-mysql.sh
@@ -27,14 +27,12 @@ VALIDATE(){
     fi
 }
 
-dnf module disable redis -y &>> $LOGS_FILE
-dnf module enable redis:7 -y &>> $LOGS_FILE
-dnf install redis -y &>> $LOGS_FILE
-VALIDATE $? "Installing Redis:7"
- 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Allowing remote connections"
+dnf install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing MySQL Server"
 
-systemctl enable redis &>> $LOGS_FILE
-systemctl start redis &>> $LOGS_FILE
-VALIDATE $? "Started Redis"
+systemctl enable mysqld &>> $LOGS_FILE
+systemctl start mysqld  &>> $LOGS_FILE
+VALIDATE $? "Enable and start MySQL server"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up root password"
