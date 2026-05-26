996a718741aafe29243a28970d31c6a5c1d0140b
joindevopscloud <info@joindevops.com>
2026-05-26 08:25:12 +0530

    api doc added

--- 04-rabbitmq.sh
+++ 04-rabbitmq.sh
@@ -30,13 +30,13 @@ VALIDATE(){
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 VALIDATE $? "Adding rabbitmq repo"
 
-dnf install rabbitmq-server -y
+dnf install rabbitmq-server -y &>> $LOGS_FILE
 VALIDATE $? "Installing rabbitmq server"
 
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl enable rabbitmq-server &>> $LOGS_FILE
+systemctl start rabbitmq-server &>> $LOGS_FILE
 VALIDATE $? "Enabling and starting rabbitmq server"
 
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop roboshop123 &>> $LOGS_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGS_FILE
 VALIDATE $? "setting up username and password"
